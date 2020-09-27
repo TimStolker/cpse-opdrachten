f6cf306eaedff85f71aa0118291939c436a06e3a
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-09-27 19:19:05 +0200

    Update main.cpp

--- week4/opdracht3/main.cpp
+++ week4/opdracht3/main.cpp
@@ -25,14 +25,23 @@ int main( void ){
    f2 << ".text\n";
    f2 << ".align 2\n" << "\n";
    f2 << "asc:\n";
-   f2 << "   .asciz \"" ;
+   f2 << "  .asciz \"" ;
 
    compressor.compress( 
       [ &f1 ]()-> int { auto c = f1.get(); return f1.eof() ? '\0' : c; },
-      [ &f2 ]( char c ){ f2.put( c ); }
-  );
+      [ &f2 ]( char c ){
+         if(c == '\n'){
+            f2.put( '\\');
+            f2.put( 'n' );
+         }
+         else{ 
+            f2.put( c );
+         }
+         
+      }
+   );
 
-   f2 << "\" " ;
+   f2 << "\"" ;
    
    f1.close();
    f2.close();
