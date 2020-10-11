1011f7184d4253abed2684ca5d9571fc2f6aa448
TimStolker <60575792+TimStolker@users.noreply.github.com>
2020-10-11 12:43:46 +0200

    Create template.hpp

---
+++ week5/opdracht2/template.hpp
@@ -0,0 +1,62 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+#include <array>
+#include <iostream>
+
+template< typename T, int N >
+class my_array {
+private:
+    std::array< T, N > arr;
+    int arraySize = 0;
+
+public:
+    void add( T var ){
+        if(arraySize==N){               //check vol
+            return;
+        }
+        for( const auto & x : arr ){     //loop door de array heen
+            if(x==var){
+                return;
+            }
+        }
+        arr[ arraySize ] = var;
+        arraySize+=1;
+
+    }
+
+    void remove(T var){                 
+        for( int i=0; i<arraySize; i++ ){
+            if(arr[i] == var){
+                arraySize -= 1;
+                for(int j=i; j<arraySize; j++){
+                    arr[j] = arr[j+1];
+                }
+                break;  
+            }
+        }
+    }
+
+    bool contains(T var){
+        for( const auto & x : arr ){     //loop door de array heen
+            if(x==var){
+                return true;             //true als het nummer erin zit
+            }
+        }
+        return false;
+    }
+
+    friend std::ostream &operator<<( std::ostream & lhs, const my_array & rhs){
+        lhs << "{";
+        for( int i = 0; i < rhs.arraySize; i++ ){     //loop door de array heen
+            lhs << rhs.arr[ i ];
+            if(!(i==rhs.arraySize-1)){                //check voor de laatste in de array om daar geen ',' achter te zetten
+                lhs << ",";
+            }
+        }
+        lhs << "}";
+        return lhs;
+    }
+
+};
+
+#endif
